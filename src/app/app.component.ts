dd6c2ccf02be859f139b861f12764a40d064eafb
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-01 18:21:05 +0100

    sesion 1 cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ab-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-intro-vitae-enxenio';
+  title = 'Enxenio';
+  constructor() {}
 }
