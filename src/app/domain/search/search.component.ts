f049a42c75f3d703962dd30c78e94207fd75c455
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-10 18:34:59 +0100

    search empty

--- src/app/domain/search/search.component.ts
+++ src/app/domain/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Item } from 'src/app/core/models/item';
 import { SearchService } from './search.service';
 
@@ -11,14 +12,20 @@ export class SearchComponent implements OnInit {
   searchTerm: string = '';
   items: Item[];
 
-  constructor(private service: SearchService) {}
+  constructor(private router: Router, private route: ActivatedRoute, private service: SearchService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.searchTerm = this.route.snapshot.queryParams.term;
+  }
 
   search() {
-    console.log('buscado ... ' + this.searchTerm);
-    this.service.getItemsBySearchTerm$(this.searchTerm).subscribe({
-      next: (items) => (this.items = items),
-    });
+    if (!this.searchTerm) {
+      this.items = [];
+    } else {
+      this.service.getItemsBySearchTerm$(this.searchTerm).subscribe({
+        next: (items) => (this.items = items),
+      });
+      this.router.navigate(['search'], { queryParams: { term: this.searchTerm } });
+    }
   }
 }
