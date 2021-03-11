22a04252eb217edb96160c0e78ccbed91529384c
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-11 16:02:46 +0100

    empty service

--- src/app/domain/categories/new/new.service.ts
+++ src/app/domain/categories/new/new.service.ts
@@ -1,16 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { ApiCategory, Category } from 'src/app/core/models/category';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NewService {
-  private endPoint = environment.apiUrl + '/categories/';
-  constructor(private http: HttpClient) {}
-  postNewCategory$(category: Category) {
-    return this.http.post<ApiCategory>(this.endPoint, category).pipe(map((response) => response.data));
-  }
-}
+export class NewService {}
