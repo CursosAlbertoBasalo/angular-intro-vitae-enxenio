0aacdb6e2f0983c6dd4dcd6c4da5d7528b3d4e5c
Alberto Basalo <albertobasalo@hotmail.com>
2021-03-17 17:38:56 +0100

    canLoad

--- src/app/domain/items/items-routing.module.ts
+++ src/app/domain/items/items-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsComponent } from './items.component';
+import { NewGuard } from './new/new.guard';
 
 const routes: Routes = [
   { path: '', component: ItemsComponent },
-  { path: 'new', loadChildren: () => import('./new/new.module').then((m) => m.NewModule) },
+  {
+    path: 'new',
+    canLoad: [NewGuard],
+    loadChildren: () => import('./new/new.module').then((m) => m.NewModule),
+  },
   { path: ':id', loadChildren: () => import('./view/view.module').then((m) => m.ViewModule) },
 ];
 
--- src/app/domain/items/new/new-routing.module.ts
+++ src/app/domain/items/new/new-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewComponent } from './new.component';
-import { NewGuard } from './new.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: NewComponent,
-    canLoad: [NewGuard],
   },
 ];
 
